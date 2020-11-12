06a907ef32a7fc829d16798f4784b7deb7f3e409
Sazzad Hossain <hossain.sakib26@hotmail.com>
2020-11-12 14:11:01 +0600

    bootstrap page complete

---
+++ WebCoursePractice/WebCoursePractice/myJS/firstJS.js
@@ -0,0 +1,15 @@
+﻿var firstNumber = 12;
+var secondNumber = 10;
+
+secondNumber *= firstNumber;
+
+console.log(secondNumber);
+
+var firstName = prompt();
+var secondName = prompt();
+
+//var name = getFullName() {
+var fullName = firstName + " " + secondName;
+//}
+
+alert(fullName);
